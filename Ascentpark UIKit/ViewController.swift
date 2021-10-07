b40f28010ac0f6fbde40886f62218812fabdb701
Nishant Minerva <nishantcafe@gmail.com>
2021-10-07 11:55:31 +0530

    Initial Commit

---
+++ Ascentpark UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ascentpark UIKit
+//
+//  Created by Satyam Kumar on 07/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ascentpark UIKitTests/Ascentpark_UIKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ascentpark_UIKitTests.swift
+//  Ascentpark UIKitTests
+//
+//  Created by Satyam Kumar on 07/10/21.
+//
+
+import XCTest
+@testable import Ascentpark_UIKit
+
+class Ascentpark_UIKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
